4127cc3a12a816c03cd8997f0876fa2a03ec9265
Dinara <dinara198989@bk.ru>
2022-05-29 16:59:57 +0100

    remove task

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
-const tasks1 = [
-    { id: 1, title: "HTML&CSS", isDone: true },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "ReactJS", isDone: false }
-]
-const tasks2 = [
-    { id: 1, title: "Harry Potter", isDone: true },
-    { id: 2, title: "Pride and Prejudice", isDone: false },
-    { id: 3, title: "Uliss", isDone: false }
-]
-
 function App() {
+
+    let tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "Redux", isDone: true }
+    ]
+    // const tasks2 = [
+    //     { id: 1, title: "Harry Potter", isDone: true },
+    //     { id: 2, title: "Pride and Prejudice", isDone: false },
+    //     { id: 3, title: "Ulysses", isDone: false }
+    // ]
+    //Оствновилась на UseState и Counter
+        useState(tasks, removeTask());
+
+
+    function removeTask(id: number) {
+        tasks = tasks.filter( t => t.id !== id)
+    }
+
     return (
         <div className="App">
-            <Todolist title = "What to learn" tasks={tasks1}/>
-            <Todolist title = "Books" tasks={tasks2}/>
+            <Todolist title = "What to learn"
+                      tasks={tasks}
+                      removeTask={removeTask}
+            />
+            {/*<Todolist title = "Books" tasks={tasks2}/>*/}
         </div>
     );
 }
