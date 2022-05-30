0025a0260ceb868dd4179e498b233b3d75f59c18
Dinara <dinara198989@bk.ru>
2022-05-30 17:54:55 +0100

    Counter + useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
+// Счетчик
+// export function Counter() {
+//     console.log("Counter rendered")
+//     let arr = useState(5);
+//     let data = arr[0]
+//     let setData = arr[1]
+//
+//     return <div onClick={ () => { setData(data+1)} }>{data}</div>
+// }
 function App() {
 
-    let tasks = [
+    let initTasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
@@ -15,12 +24,15 @@ function App() {
     //     { id: 2, title: "Pride and Prejudice", isDone: false },
     //     { id: 3, title: "Ulysses", isDone: false }
     // ]
-    //Оствновилась на UseState и Counter
-        useState(tasks, removeTask());
 
+
+    let arr =  useState(initTasks);
+    let tasks = arr[0];
+    let setTasks = arr[1];
 
     function removeTask(id: number) {
-        tasks = tasks.filter( t => t.id !== id)
+        let filteredTasks = tasks = tasks.filter( t => t.id !== id)
+        setTasks(filteredTasks);
     }
 
     return (
@@ -35,3 +47,4 @@ function App() {
 }
 
 export default App;
+
