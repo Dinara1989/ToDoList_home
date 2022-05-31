bca93f0a70d1998d213389dfc08a31fa5eef17e6
Dinara <dinara198989@bk.ru>
2022-05-31 19:46:06 +0100

    add questions for exams

--- src/App.tsx
+++ src/App.tsx
@@ -53,3 +53,76 @@ function App() {
 
 export default App;
 
+
+// import ReactDOM from 'react-dom'
+//
+// export const VideoHeader = (props: {videoName: string}) => {
+//     return <div>
+//         😀 {props.videoName}
+//     </div>
+// }
+// export const VideoContent = (props: {videoContent: string}) => {
+//     return <div>
+//         📼 <a href={props.videoContent}>{props.videoContent}</a>
+//     </div>
+// }
+// export const VideoDescription = (props: {videoDescription: string}) => {
+//     return <div>
+//         📑 {props.videoDescription}
+//     </div>
+// }
+//
+// export const YoutubeVideo = (props: any) => {
+//     return <div>
+//         <VideoHeader xxx />
+//         <VideoContent yyy />
+//         <VideoDescription zzz />
+//     </div>
+// }
+//
+// export const App = () => {
+//     const video = {
+//         title: 'Samurai way',
+//         link: 'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8',
+//         description: 'Best free react-course'
+//     }
+//
+//     return <YoutubeVideo video={video} />
+// }
+//
+// ReactDOM.render(<App />,
+//     document.getElementById('root')
+// );
+//
+// import ReactDOM from 'react-dom'
+//
+// const Son = (props: any) => {
+//     return <div>
+//         I am son. My name is {props.name}
+//     </div>
+// }
+//
+//
+// const Father = (props: any) => {
+//     return <div>
+//         I am father. My name is {props.name}
+//         <Son name={props.sonName} />
+//     </div>
+// }
+//
+// const Granny = (props: any) => {
+//     return <div>
+//         I am granny. My name is {props.name}
+//         <Father name={props.fatherName} sonName={props.sonName} />
+//     </div>
+// }
+//
+// export const App = () => {
+//     return <div>
+//         <Granny XXX={'Бабуля'} YYY={'Батя'} ZZZ={'Сын'}/>
+//     </div>
+// }
+//
+// ReactDOM.render(<App/>,
+//     document.getElementById('root')
+// )
