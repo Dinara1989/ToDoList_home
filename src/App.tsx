9b0b5964bbdc259039c061dcc77795bfcc3bbc4d
Dinara <dinara198989@bk.ru>
2022-10-16 15:42:40 +0100

    Lesson 5. Refactoring+remove Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,121 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Todolist} from "./components/Todolist";
-import Counter from "./components/counter/Counter";
-import {v1} from "uuid";
+import {v1} from 'uuid';
+import {Todolist} from "./Todolist";
 
 export type FilterValuesType = "all" | "active" | "completed";
+type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
 
-const App = () => {
+function App() {
 
-    let [tasks, setTasks] = useState([
-        { id: v1(), title: "CSS & HTML", isDone: true},
-        { id: v1(), title: "JS", isDone: false},
-        { id: v1(), title: "REACT", isDone: true},
-        { id: v1(), title: "JS", isDone: false},
-        { id: v1(), title: "REACT", isDone: true}
-    ]);
-    let [filter,setFilter] = useState<FilterValuesType>("all")
-
-    function removeTask(id: string) {
-        let filteredTasks = tasks.filter( t => t.id !== id)
-        setTasks(filteredTasks);
+    function removeTask(id: string, todolistID: string) {
+        let tasks = tasksObj[todolistID];
+        let filteredTasks = tasks.filter(t => t.id != id);
+        tasksObj[todolistID] = filteredTasks;
+        setTasks({...tasksObj});
+        //or
+        // let filteredTasks = {...tasks,[ID_td]:tasks[ID_td].filter(t => t.id !== id)}
+        // setTasks(filteredTasks);
     }
-    function addTask(title:string) {
-        let newTask = {
-            id: v1(),
-            title: title,
-            isDone: false
-        };
-        let newTasksTitle = [newTask, ...tasks];
-        setTasks(newTasksTitle);
+
+    function addTask(title: string, todolistID: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let tasks = tasksObj[todolistID];
+        let newTasks = [task, ...tasks];
+        tasksObj[todolistID] = newTasks;
+        setTasks({...tasksObj});
+        // let task = {id: v1(), title: title, isDone: false};
+        // let newTasks = [task, ...tasks];
+        // setTasks(newTasks);
     }
-    function changeStatus(taskId:string, isDone:boolean) {
-        let task = tasks.find( t => t.id === taskId);
+
+    function changeStatus(taskId: string, isDone: boolean, todolistID: string) {
+        let tasks = tasksObj[todolistID];
+        let task = tasks.find(t => t.id === taskId);
         if (task) {
             task.isDone = isDone;
+            setTasks({...tasksObj});
         }
-        setTasks([ ...tasks ]);
-    }
-    let tasksForTodolist = tasks;
-
-    if (filter === "active") {
-        tasksForTodolist = tasks.filter(t => t.isDone === false);
-    }
-    if (filter === "completed") {
-        tasksForTodolist = tasks.filter(t => t.isDone === true);
     }
 
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value);
+    function changeFilter(value: FilterValuesType, todolistID: string) {
+        let todolist = todolists.find(tl => tl.id === todolistID);
+        if(todolist) {
+            todolist.filter = value;
+            setTodolists([...todolists])
+        }
+    }
+    //or
+    // function changeFilter(value: FilterValuesType) {
+    //     setFilter(value);
+    // }
+
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let [todolists, setTodolists] = useState <Array<TodolistType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'active'},
+        {id: todolistID2, title: 'What to buy', filter: 'completed'}
+    ])
+
+    let removeTodolist = (todolistsID: string) => {
+        let filteredTodolist = todolists.filter(tl => tl.id !== todolistsID)
+        setTodolists(filteredTodolist);
+        delete tasksObj[todolistsID];
+        setTasks({...tasksObj});
     }
 
+    let [tasksObj, setTasks] = useState({
+        [todolistID1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "Bread", isDone: true},
+            {id: v1(), title: "Honey", isDone: false},
+            {id: v1(), title: "Cola", isDone: false},
+            {id: v1(), title: "Water", isDone: false},
+        ],
+    });
 
     return (
-        <div>
-            <Todolist title={"What to learn"}
-                      tasks={tasksForTodolist}
-                      removeTask={removeTask}
-                      changeFilter={changeFilter}
-                      addTask={addTask}
-                      changeTaskStatus={changeStatus}
-                      filter={filter}
-            />
-            {/*<Todolist title={"What to do"} tasks={tasks}/>*/}
-            {/*<Todolist title={"What to watch"}/>*/}
-            <Counter/>
+        <div className="App">
+            {
+             todolists.map( (tl)=> {
+
+                 let tasksForTodolist = tasksObj[tl.id];
+
+                 if (tl.filter === "active") {
+                     tasksForTodolist = tasksForTodolist.filter(t => t.isDone === false);
+                 }
+                 if (tl.filter === "completed") {
+                     tasksForTodolist = tasksForTodolist.filter(t => t.isDone === true);
+                 }
+
+                 return <Todolist
+                     key={tl.id}
+                     id={tl.id}
+                     title={tl.title}
+                     tasks={tasksForTodolist}
+                     removeTask={removeTask}
+                     changeFilter={changeFilter}
+                     addTask={addTask}
+                     changeTaskStatus={changeStatus}
+                     filter={tl.filter}
+                     removeTodolist={removeTodolist}
+                 />
+             })
+            }
         </div>
     );
-};
+}
 
 export default App;
