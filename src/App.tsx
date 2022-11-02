d2e303a6b9878951c41054e6748bd6c6f5e03f09
Dinara <dinara198989@bk.ru>
2022-11-02 21:27:20 +0100

    lesson 8. Reducer + unit tests for todolists

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {AddItemForm} from "./AddItemForm";
 import Counter from "./counter/Counter";
 
 export type FilterValuesType = "all" | "active" | "completed";
-type TodolistType = {
+export type TodolistType = {
     id: string
     title: string
     filter: FilterValuesType
@@ -77,8 +77,8 @@ function App() {
     ])
 
     let removeTodolist = (todolistsID: string) => {
-        let filteredTodolist = todolists.filter(tl => tl.id !== todolistsID)
-        setTodolists(filteredTodolist);
+        // let filteredTodolist = todolists.filter(tl => tl.id !== todolistsID)
+        // setTodolists(filteredTodolist);
         delete tasksObj[todolistsID];
         setTasks({...tasksObj});
     }
